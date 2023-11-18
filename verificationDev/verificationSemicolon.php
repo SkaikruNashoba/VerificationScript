4c59d2cb74f419e08d8ab3b2dd3de1d63c1f3740
SkaikruNashoba <valentin.leleu@epitech.eu>
2023-11-18 01:08:33 +0100

    fix name typo

--- verificationDev/verificationSemicolon.php
+++ verificationDev/verificationSemicolon.php
@@ -8,8 +8,14 @@
  * @param int $fileCount A reference to the total file count.
  * @param string $parentPath The relative path of the parent directory (used for recursion).
  */
+$argTwo = $argv[2];
+$argThree = $argv[3];
+$directoryPath = $argv[1];
+$fileCount = 0;
+$lineError = 0;
 
-function exploreDirectory($directoryPath, $argTwo, $argThree, &$fileCount, &$lineError, $parentPath = '') {
+
+function exploreDirectory($directoryPath, &$argTwo, &$argThree, &$fileCount, &$lineError, $parentPath = '') {
 	if (!isset($directoryPath)) {
 		echo "\033[31mPlease indicate the path of the folder to analyze.\033[0m\n";
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
@@ -39,21 +45,22 @@ function exploreDirectory($directoryPath, $argTwo, $argThree, &$fileCount, &$lin
 							}
 							$trimmedLine = rtrim($line);
 							if (
-								in_array(substr($trimmedLine, -1), ['}', ')', '\'', '"'])
-								&& substr($trimmedLine, -2) !== '}}'
-								&& !preg_match("/\{[\w]+\}*/", $trimmedLine)
+								in_array(substr($trimmedLine, -1), ["}", ")", "\'", "\""])
+								&& substr($trimmedLine, -2) !== "}}"
+								&& substr($trimmedLine, -2) !== ")}"
+								&& substr($trimmedLine, -2) !== "\')"
+								&& !preg_match("/\{[\w\W]+\}*/", $trimmedLine)
 								&& !preg_match("/return(?! \()/", $trimmedLine)
 							) {
 								if (isset($argTwo) && $argTwo === '-noEdit') {
-									echo "\033[31mPotential missing a selemicon at this line (line " . ($lineNumber - 1) . ")\033[0m\n";
-									$lineError++;
+									echo "\033[31mPotential missing a semicolon at this line (line " . ($lineNumber) . ") of $relativePath\033[0m\n";
 									$listOfLine[] = $lineNumber - 1;
 								} else {
 									$trimmedLine .= ';';
-									echo "Modified line $lineNumber of file $filePath\n";
-									$lineError++;
 									$listOfLine[] = $lineNumber - 1;
+									echo "Modified line $lineNumber of file $filePath\n";
 								}
+								$lineError++;
 							}
 							$lineNumber++;
 							$newContents .= $trimmedLine . PHP_EOL;
@@ -64,19 +71,18 @@ function exploreDirectory($directoryPath, $argTwo, $argThree, &$fileCount, &$lin
 							}
 						}
 						if (isset($argThree) && $argThree === "-withoutExplain") {
-							echo "\n\033[33m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m";
-							echo "\nLines affected:\n[ ";
+							echo "Lines affected:\n[ ";
 							foreach ($listOfLine as $line) {
-								echo " \033[31m$line\033[0m,";
+								echo " \033[31m" . ($line + 1) . "\033[0m,";
 							}
-							echo " ]";
+							echo " ]\n\n";
 						}
 						file_put_contents($filePath, $newContents);
 					} else {
 						echo "\033[31mFile $filePath is not a js or php file.\033[0m\n";
 					}
 				} elseif (is_dir($filePath)) {
-					exploreDirectory($argTwo, $argThree, $filePath, $fileCount, $lineError, $relativePath);
+					exploreDirectory($directoryPath, $argTwo, $argThree, $filePath, $fileCount, $lineError, $relativePath);
 				}
 			}
 		}
@@ -88,12 +94,6 @@ function exploreDirectory($directoryPath, $argTwo, $argThree, &$fileCount, &$lin
 
 $startTime = microtime(true);
 
-$directoryPath = $argv[1];
-$argTwo = $argv[2];
-$argThree = $argv[3];
-$fileCount = 0;
-$lineError = 0;
-
 exploreDirectory($directoryPath, $argTwo, $argThree, $fileCount, $lineError);
 
 $endTime = microtime(true);
@@ -101,7 +101,7 @@ $executionTime = round($endTime - $startTime, 2);
 
 echo "\n\n\033[33m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m\n";
 echo "Total files rewrite: \033[32m$fileCount\033[0m\n";
-echo ($lineError === 0) ? ("Total lines where a selemicon was missing: \033[32m$lineError\033[0m\n") : ("Total lines where a selemicon was missing: \033[31m$lineError\033[0m\n");
+echo ($lineError === 0) ? ("Total lines where a semicolon was missing: \033[32m$lineError\033[0m\n") : ("Total lines where a semicolon was missing: \033[31m$lineError\033[0m\n");
 echo "Execution time: \033[32m$executionTime\033[0m seconds\n";
 echo "\033[33m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m\n";
 
